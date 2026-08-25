d8bb860c61481d16d11c3404b776bc4353c64387
Anish kumar maurya <anishmaurya8200@gmail.com>
2026-08-26 00:38:31 +0530

    second commit

---
+++ anishdata.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE ANISH1;
+CREATE DATABASE anish2;
+DROP DATABASE ANISH1;
+DROP DATABASE ANISH1;
+create database sudent;
+use student;
+create table st(id int primary key,name varchar(100),email varchar(100),password int not null);
+show tables;
+show databases;
+use student;
+show tables;
+select*from st;
+use sudent;
+create table st(id int primary key,name varchar(100),email varchar(100),password int not null);
+
+use yt_anish;
+show tables;
+
+
+
+
+
+
+
+
+
+
